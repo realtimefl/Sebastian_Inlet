522bf2bb2974d9a1da1f86e0d6a0891ac461432b
ahsan07habib22 <mhabib2014@my.fit.edu>
2020-12-16 16:00:14 -0500

    update commit

---
+++ git_upload.sh
@@ -0,0 +1,28 @@
+
+git init
+git pull origin master 
+git remote add origin https://github.com/realtimefl/Sebastian_Inlet.git
+rm waterlevel_evergladesinlet.jpg waterlevel_hillsboroinlet.jpg waterlevel_plantriver.jpg waterlevel_porteverglades.jpg
+
+git add .
+git commit -m "update commit"
+git push -u origin master
+
+# use 'git pull origin master' command if 'error: failed to push some refs to ... ' appears
+
+echo copying files
+
+cp -r /home/admin/sebastian/plots/waterlevel_lobo.jpg  /home/admin/sebastian/githubs/Sebastian_Inlet-main
+
+cp -r /home/admin/sebastian/plots/waterlevel_sebastian_inlet.jpg  /home/admin/sebastian/githubs/Sebastian_Inlet-main
+
+cp -r /home/admin/sebastian/plots/waterlevel_northjetty.jpg  /home/admin/sebastian/githubs/Sebastian_Inlet-main
+
+
+echo updating github
+
+git init 
+git pull origin master
+git add .
+git commit -m "update commit"
+git push -u origin master
