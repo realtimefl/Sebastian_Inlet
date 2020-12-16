a960ac99bf17fd8771aaecaab96bdeea2b46e464
ahsan07habib22 <mhabib2014@my.fit.edu>
2020-12-16 16:01:42 -0500

    update commit

--- git_upload.sh
+++ git_upload.sh
@@ -1,7 +1,7 @@
 
 git init
 git pull origin master 
-git remote add origin https://github.com/realtimefl/Sebastian_Inlet.git
+#git remote add origin https://github.com/realtimefl/Sebastian_Inlet.git
 rm waterlevel_evergladesinlet.jpg waterlevel_hillsboroinlet.jpg waterlevel_plantriver.jpg waterlevel_porteverglades.jpg
 
 git add .
